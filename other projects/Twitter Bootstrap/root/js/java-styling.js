7c49228bab702a033857a0b9a3558125c58170a4
menesesrichard <Rich@Richards-MacBook-Pro.local>
2016-02-02 10:47:22 -0500

    Twitter Bootstrap

---
+++ other projects/Twitter Bootstrap/root/js/java-styling.js
@@ -0,0 +1,15 @@
+var userAge = prompt("Please enter your age");
+    
+if (userAge == 0)
+{
+   alert("So you're a baby!");
+}
+else if ( userAge < 0 | userAge > 200)
+    {
+   alert("I think you may be lying about your age");
+    }
+else
+
+   alert("That's a good age");
+}
+

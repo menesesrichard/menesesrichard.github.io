fe5e77ad885061b2973d9780df58ea54848b7532
menesesrichard <Rich@Richards-MacBook-Pro.local>
2016-03-15 12:20:00 -0400

    json

---
+++ other projects/Ajax-examples/js/clients.js
@@ -0,0 +1,30 @@
+
+$("#getClients").on("click", function() {
+    
+    var url = "http://andersonguelphis.github.github.io/otherProjects//Ajax-examples/clients.html"
+    
+    $.getJSON(url, function(data){
+        
+        var html = "<table class='table table-hover table-stripe>" +
+            "<tr><th></th><th></th><th>Company</th></tr>";
+        
+        
+        $.each(data, function(index, item){
+           html += "<tr> + 
+               <td>"+item.name+"</td>
+               <td>"+item.email"</td>
+               <td>"+item.company+"</td>
+               </tr>";
+        
+        
+        html += "</table>";
+        $("#data").append
+        //alert(data);
+        //console.dir(data);
+            
+        })//get JSON
+    
+})//click
+               
+
+})//document ready
